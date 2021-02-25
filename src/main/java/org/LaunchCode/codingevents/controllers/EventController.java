ae49937647073b2591d56075e5d68f58532505b7
jhall766 <Jhall766@gmail.com>
2021-02-24 21:34:43 -0600

    Added Complete MVC Pattern

--- src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
@@ -1,10 +1,11 @@
 package org.LaunchCode.codingevents.controllers;
 
+import org.LaunchCode.codingevents.data.EventData;
+import org.LaunchCode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events",events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -27,9 +27,24 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@ModelAttribute Event newEvent){
+        EventData.add(newEvent);
         return  "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title","Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds){
+        for (int id: eventIds){
+            EventData.remove(id);
+        }
+        return "redirect:";
     }
 
 }
